217e2560f9ffd8fc7584c5912cba5cf7e8da0491
Alina Chirila <67912483+alina-chr@users.noreply.github.com>
2021-10-06 14:37:36 +0300

    2:37 - not ok

--- src/components/GetWeather.jsx
+++ src/components/GetWeather.jsx
@@ -12,27 +12,32 @@ const GetWeather = () => {
     country: '',
   });
 
-  const [weather, setWeather] = useState([]);
+  const [weather, setWeather] = useState(null);
+  const [notFound, setNotFound] = useState(false);
   async function getWeatherData(event) {
     event.preventDefault();
-    if (form.city === ''){
-      alert('Add a city')
+    if (form.city === '') {
+      alert('Add a city');
     } else {
-      try {const data = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&units=metric&appid=${apiKey}`,
-      )
-        .then((response) => response.json())
-        .then((data) => data);
-        debugger
-      setWeather({
-        data: data,
-      }) } catch (error) {
-        console.log(error)
-      } 
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&units=metric&appid=${apiKey}`,
+        );
+          if (response.status=('404')) { throw new Error ('404') }
+
+        const  data  = await response.json()
+
+        ;
+        setWeather({
+          data: data,
+        });
+      } catch (error) {
+        if (error.message === '404'){
+          setNotFound(true)
+        };
+      }
     }
   }
-
-
 
   const handleChange = (event) => {
     let name = event.target.name;
@@ -47,57 +52,70 @@ const GetWeather = () => {
     }
   };
 
-
-return (
-  <>
-  <div className="weather-form" css={css`text-align: center; `}>
-          <form onSubmit={(event) => getWeatherData(event)}>
+  return (
+    <>
+      <div
+        className="weather-form"
+        css={css`
+          text-align: center;
+        `}
+      >
+        <form onSubmit={getWeatherData}>
           <input
             type="text"
             name="city"
             placeholder="City"
             onChange={(event) => handleChange(event)}
-            css={css`width: 20%;
-            padding: 10px 20px;
-            font-weight:bold;
-            border-width: 1px;
-            margin-right: 5px;
-            background: rgba(255, 255, 255, 0.3);
-            box-sizing: border-box;`}
+            css={css`
+              width: 20%;
+              padding: 10px 20px;
+              font-weight: bold;
+              border-width: 1px;
+              margin-right: 5px;
+              background: rgba(255, 255, 255, 0.3);
+              box-sizing: border-box;
+            `}
           ></input>
           <input
             type="country"
             name="country"
             placeholder="Country"
             onChange={(event) => handleChange(event)}
-            css={css`width: 20%;
-            padding: 10px 20px;
-            border-width: 1px;
-            background: rgba(255, 255, 255, 0.3);
-            margin-right: 5px;
-            box-sizing: border-box;`}
+            css={css`
+              width: 20%;
+              padding: 10px 20px;
+              border-width: 1px;
+              background: rgba(255, 255, 255, 0.3);
+              margin-right: 5px;
+              box-sizing: border-box;
+            `}
           ></input>
-          <button type="submit" css={css`
-      background-color: transparent;
-      border: 1 px solid white;
-      padding: 10px 15px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 15px;
-      outline: none;
-      background: rgba(255, 255, 255, 0.3);
-      border-width: 1px;
-    `}>GET WEATHER</button>
-  </form>
-  {weather.data !== undefined ? (
+          <button
+            type="submit"
+            css={css`
+              background-color: transparent;
+              border: 1 px solid white;
+              padding: 10px 15px;
+              text-align: center;
+              text-decoration: none;
+              display: inline-block;
+              font-size: 15px;
+              outline: none;
+              background: rgba(255, 255, 255, 0.3);
+              border-width: 1px;
+            `}
+          >
+            GET WEATHER
+          </button>
+        </form>
+        {weather?.data && !notFound ? (
           <div>
             <DisplayWeather data={weather.data}></DisplayWeather>
           </div>
-        ) : null}
- </div>
- </>
-)
-}
+        ) : <p>Error</p>}
+      </div>
+    </>
+  );
+};
 
 export default GetWeather;
